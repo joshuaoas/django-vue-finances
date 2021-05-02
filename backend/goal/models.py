fe644192d078cbfc218f636ddb576dc2f8cabe8f
Josh <joas@spacesaver.com>
2021-05-02 13:57:41 -0500

    goal model

--- backend/goal/models.py
+++ backend/goal/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import datetime, date
 
-# Create your models here.
+
+class Goal(models.Model):
+    title = models.CharField(max_length=256)
+    savings_needed = models.FloatField(
+        default=0, verbose_name='Savings needed')
+    date = models.DateField(
+        auto_now_add=False, auto_now=False, blank=True, null=True)
+    timestamp = models.DateField(auto_now_add=True, auto_now=False, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name_plural = "goals"
+
+    def __str__(self):
+        return self.source
