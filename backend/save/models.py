6f31cc4ff1a5de842eb931339c88a98de815c1a4
Josh <joas@spacesaver.com>
2021-05-02 14:03:43 -0500

    created save model and added to admin

--- backend/save/models.py
+++ backend/save/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import datetime, date
 
-# Create your models here.
+
+class Save(models.Model):
+    source = models.CharField(max_length=30)
+    savings = models.FloatField(default=0, verbose_name='Savings')
+    date = models.DateField(
+        auto_now_add=False, auto_now=False, blank=True, null=True)
+    timestamp = models.DateField(auto_now_add=True, auto_now=False, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name_plural = "savings"
+
+    def __str__(self):
+        return self.source
