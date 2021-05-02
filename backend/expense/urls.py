629ce7fafdfd4c5865c9f512c42eba5598085008
Josh <joas@spacesaver.com>
2021-05-02 13:54:37 -0500

    Made API expense viewset and URLS

---
+++ backend/expense/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+
+from rest_framework.routers import DefaultRouter
+
+from .views import ExpenseViewSet
+
+router = DefaultRouter()
+
+router.register('expense', ExpenseViewSet, basename='expense')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
--- backend/expense/views.py
+++ backend/expense/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets
+
+from .models import Expense
+from .models import Category
+
+from .serializers import ExpenseSerializer
+from .serializers import CategorySerializer
+
+
+class ExpenseViewSet(viewsets.ModelViewSet):
+    serializer_class = ExpenseSerializer
+    queryset = Expense.objects.all()
+
+    # only get Expenses you created
+    def get_queryset(self):
+        return self.queryset.filter(created_by=self.request.user)
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    serializer_class = CategorySerializer
+    queryset = Category.objects.all()
+
+    # only get categories you created
+    def get_queryset(self):
+        return self.queryset.filter(created_by=self.request.user)
