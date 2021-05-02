0b1a2a76e962457aab8c6bda538fb5b0d12eff30
Josh <joas@spacesaver.com>
2021-05-02 14:00:12 -0500

    created income model, added to admin, and migrate

--- backend/income/models.py
+++ backend/income/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import datetime, date
 
-# Create your models here.
+
+class Income(models.Model):
+    source = models.CharField(max_length=30)
+    income = models.FloatField(default=0, verbose_name='Income')
+    date = models.DateField(
+        auto_now_add=False, auto_now=False, blank=True, null=True)
+    timestamp = models.DateField(auto_now_add=True, auto_now=False, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name_plural = "income"
+
+    def __str__(self):
+        return self.source
