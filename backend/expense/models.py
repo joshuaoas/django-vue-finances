f8bfd47489f392328d9f453a9e2d1467f3e1e214
Josh <joas@spacesaver.com>
2021-05-02 13:34:37 -0500

    Created expense and category models

--- backend/expense/models.py
+++ backend/expense/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
+from django.db import models
+from django.contrib.auth.models import User
+from datetime import datetime, date
+
 # Create your models here.
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=30)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+
+    class Meta:
+        verbose_name_plural = "categories"
+
+    def __str__(self):
+        return self.title
+
+
+class Expense(models.Model):
+    title = models.CharField(max_length=30,)
+    cost = models.FloatField(verbose_name='total cost', default=0)
+    description = models.TextField(null=True, blank=True, )
+    date = models.DateField(
+        auto_now_add=False, auto_now=False, blank=True, null=True)
+    timestamp = models.DateField(auto_now_add=True, auto_now=False, blank=True)
+
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name_plural = "expenses"
+
+    def __str__(self):
+        return self.title
